b9058904e522d5406ae7892730b5943d8ff37b5a
Drishya Koirala <drishyakoirala@gmail.com>
2026-06-09 13:39:47 -0500

    fixed tests to use mock camera instead of hardware

--- cam_sample/tests/CameraTests.cpp
+++ cam_sample/tests/CameraTests.cpp
@@ -3,7 +3,7 @@
 
 TEST(CameraTests, CameraOpensSuccessfully)
 {
-    Camera cam(10);
+    MockCamera cam;
 
     ASSERT_TRUE(cam.open());
     ASSERT_TRUE(cam.isOpen());
@@ -11,7 +11,7 @@ TEST(CameraTests, CameraOpensSuccessfully)
 
 TEST(CameraTests, CameraClosesSuccessfully)
 {
-    Camera cam(10);
+    MockCamera cam;
 
     ASSERT_TRUE(cam.open());
 
